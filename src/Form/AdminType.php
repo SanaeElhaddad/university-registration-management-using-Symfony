e0554b909604604397d9f63b760273a03fac5fde
Mohammed Mouhcine <mouhcine.mohammed00@gmail.com>
2024-01-04 19:21:34 +0100

    Profil setting Admin2

---
+++ src/Form/AdminType.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Admin;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\FileType;
+use Symfony\Component\Form\Extension\Core\Type\PasswordType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class AdminType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('cin')
+            ->add('nom')
+            ->add('prenom')
+            ->add('numero')
+            ->add('email')
+            ->add('password', PasswordType::class)
+            ->add('image', FileType::class, [
+                'label' => 'Image',
+                'required' => false, 
+                'data_class' => null, 
+            ])
+            ->add('Submit', SubmitType::class);
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => Admin::class,
+        ]);
+    }
+}
