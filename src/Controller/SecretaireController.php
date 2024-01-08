1fb1607fb27b3542bef56336e40aa3ff2e75b8b5
sanae <sanae.elhaddad99@gmail.com>
2024-01-09 00:41:17 +0100

    l'integration de linterface d'affichage des etudiants

---
+++ src/Controller/SecretaireController.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use App\Repository\EtudiantRepository; 
+use App\Entity\Etudiant;
+
+
+
+class SecretaireController extends AbstractController
+{
+    #[Route('/secretaire', name: 'app_secretaire')]
+    public function index(): Response
+    {
+        return $this->render('secretaire/index.html.twig', [
+            'controller_name' => 'SecretaireController',
+        ]);
+    }
+    #[Route('/secretaire/etudiants', name:'etudiants')] 
+   public function ListEtudiant(EtudiantRepository $etudiantRepository): Response
+    {
+        $etudiants = $etudiantRepository->findAll();
+        
+        return $this->render('secretaire/etudiants.html.twig', ['etudiants' => $etudiants]);
+    }
+}
