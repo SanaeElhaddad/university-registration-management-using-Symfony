049787b1e394629ea79e836682173d8f7a3f5335
Chammi Driss <driss.chammi@usmba.ac.ma>
2024-01-09 11:06:20 +0100

    Intégrer les statistiques dans l'interface du dashboard

--- src/Controller/ResponsableController.php
+++ src/Controller/ResponsableController.php
@@ -18,8 +18,21 @@ class ResponsableController extends AbstractController
         ]);
     }
     #[Route('/dashboard', name: 'dahsboard_responsable')]
-    public function dashboard_responsable(): Response
+    public function dashboard_responsable(FaculteRepository $faculteRepository): Response
     {
-        return $this->render('responsable/index.html.twig');
+        
+        // Appel de la fonction countStudentsByFaculty du FaculteRepository
+        $resultats = $faculteRepository->countStudentsByFaculty();
+
+        // Transforme les résultats en un tableau associatif pour faciliter le passage à la vue
+        $data = [];
+        foreach ($resultats as $resultat) {
+            $data['faculte'][] = $resultat['nom_faculte'];
+            $data['etudiants'][] = $resultat['nombre_etudiants'];
+        }
+
+        $resultats2 = $faculteRepository->getPercentageStudentsByFaculty();
+
+        return $this->render('responsable/index.html.twig', ['data' => $data,'resultats' => $resultats2]);
     }
 }
